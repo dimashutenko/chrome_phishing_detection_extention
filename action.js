53a471de756e4352ea298a06590db8f2db43bbad
Dima Shutenko <58172685+dimashutenko@users.noreply.github.com>
2023-03-20 19:57:48 -0400

    Add files via upload

---
+++ action.js
@@ -0,0 +1,5 @@
+var newDiv = document.createElement("div");
+newDiv.innerHTML = "<p style='margin:200px auto 20px auto; width:750px;font-size:40px;font-weight:900'>Ваше з'єднання не є безпечним!</p><p style='margin:100px auto 100px auto; width:715px;font-size:26px;font-weight:700px'>P.S. Прогортайте нижче, якщо бажаєте продовжити</p><p style='margin:0px 50px 50px auto; width:300px;font-size:14px;font-weight:700px;'>Розроблено Dima Shutenko</p>";
+document.body.insertBefore(newDiv, document.body.firstChild);
+
+// alert("your connection is not secure");
